18ba4fdeadf60aff5326904369e1569e12818d79
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2019-06-24 18:05:46 -0300

    Refactory show info

--- src/index.js
+++ src/index.js
@@ -1,29 +1,49 @@
+String.prototype.replaceAll = function (find, replace) {
+    var str = this;
+    return str.replace(new RegExp(find.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'), 'g'), replace);
+};
+
+
 const axios = require('axios');
 const readline = require('readline');
 
 const url = "https://www.linkcorreios.com.br/";
 
-const rastramento = (codigo) => {
+const rastramento = (codigo = 'PU378066229BR') => {
 
     let path = `${url}${codigo}`;
     console.log(`URL: ${path}\n`)
     axios.get(path).then((resp)=>{
-       showInfo(resp.data);
+       processPage(resp.data);
     })
     .catch(err => {
         console.error(err);
     })
 }
 
-const showInfo = (page) => {
+const processPage = (page) => {
     let table = getTable(page)
     let trs = getTrs(table)
-    getTds(trs);
+    let linhas = getTds(trs);
+
+    printInfo(linhas);
+}
+
+const printInfo = (linhas = [])=>{
+
+    for (let index = 0; index < linhas.length; index++) {
+        const element = linhas[index];
+        const next = linhas[index + 1] || ""
+
+        console.log(`${element} | ${next}`)
+        index += 1
+    }
 }
 
 const getTds = (trs) => {
 
     linhas = []
+    saida = []
     for(tr of trs){
         let aux = tr.replaceAll('\n', '').replaceAll('\t', '').replaceAll('<td>', '').replaceAll('</td>', '')
         linhas.push(aux)
@@ -36,7 +56,7 @@ const getTds = (trs) => {
             let initType = linha.indexOf("<strong>") + 8
             let endType = linha.indexOf("</strong>")
 
-            console.log(`${linha.substring(initData, endData)} - ${linha.substring(initType, endType)}`)
+            saida.push(`${linha.substring(initData, endData)} - ${linha.substring(initType, endType)}`)
         }else if (linha.indexOf("Origem") > -1){
             let initOrigem = linha.indexOf("<tr>") + 4
             let endOrigem = linha.indexOf("Destino")
@@ -44,23 +64,20 @@ const getTds = (trs) => {
             let initDestino = linha.indexOf("Destino")
             let endDestino = linha.lenght
 
-            console.log(`${linha.substring(initOrigem, endOrigem)} - ${linha.substring(initDestino, endDestino)}`)
+            saida.push(`${linha.substring(initOrigem, endOrigem)} - ${linha.substring(initDestino, endDestino)}`)
 
         }else if(linha.indexOf("Local") > -1){
            
             let initDestino = linha.indexOf("Local:")
             let endDestino = linha.lenght
 
-            console.log(`${linha.substring(initDestino, endDestino)}`)
+            saida.push(`${linha.substring(initDestino, endDestino)}`)
 
         }
     }
+    return saida;
 }
 
-String.prototype.replaceAll = function (find, replace) {
-    var str = this;
-    return str.replace(new RegExp(find.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'), 'g'), replace);
-};
 
 const getTrs = (table) => {
     let aux = table.replace('<tbody>', '')
@@ -81,7 +98,7 @@ const leitor = readline.createInterface({
 });
 
 leitor.question("Qual o código de rastreio? ", function(answer) {
-    var resp = answer.replace('\n', '');
+    var resp = answer == "" ? undefined : answer.replace('\n', '');
     rastramento(resp);
     
     leitor.close();
